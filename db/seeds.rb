31cc9557a1bd597440809fea7b24964b4c64befe
Tiago Varela <vogait@gmail.com>
2014-12-15 20:02:21 +0000

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Recipe.create([
+  {
+    name: 'Broiled Sesame Cod',
+    submitter: 'Tiago',
+    url: 'http://allrecipes.com/recipe/broiled-sesame-cod/'
+  },
+  {
+    name: 'Grilled Cod',
+    submitter: 'Tiago',
+    url: 'http://allrecipes.com/recipe/grilled-cod/'
+  },
+  {
+    name: 'Perfect Turkey',
+    submitter: 'Pedro',
+    url: 'http://allrecipes.com/recipe/perfect-turkey/'
+  },
+])
+
+sesame_recipe = Recipe.where(name: 'Broiled Sesame Cod').first!
+Vote.create(recipe: sesame_recipe, up: true)
+Vote.create(recipe: sesame_recipe, up: true)
+Vote.create(recipe: sesame_recipe, up: true)
+Vote.create(recipe: sesame_recipe, up: true)
+Vote.create(recipe: sesame_recipe, up: false)
+Vote.create(recipe: sesame_recipe, up: false)
+
+turkey_recipe = Recipe.where(name: 'Perfect Turkey').first!
+Vote.create(recipe: turkey_recipe, up: true)
+Vote.create(recipe: turkey_recipe, up: true)
+Vote.create(recipe: turkey_recipe, up: true)
+Vote.create(recipe: turkey_recipe, up: false)
+Vote.create(recipe: turkey_recipe, up: false)
+
+grilled_recipe = Recipe.where(name: 'Grilled Cod').first!
+Vote.create(recipe: grilled_recipe, up: true)
+Vote.create(recipe: grilled_recipe, up: false)
+Vote.create(recipe: grilled_recipe, up: false)
