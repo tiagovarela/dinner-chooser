e9354954ebed36b86aa07ff91f492704be9bc6ca
Tiago Varela <vogait@gmail.com>
2014-12-15 20:03:15 +0000

    Get recipe image URL and display it

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,9 +1,19 @@
 class Recipe < ActiveRecord::Base
   has_many :votes
 
+  before_save :set_image_url
+
   validates :name, :submitter, :url, presence: true
 
   def rank
     votes.ups.count - votes.downs.count
+  end
+
+  private
+
+  def set_image_url
+    mechanize = Mechanize.new
+    page = mechanize.get(url)
+    self.image_url = page.search("//img[@id='imgPhoto']/@src").text
   end
 end
