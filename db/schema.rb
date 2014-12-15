a924e42f25064a5e52f96ca2b991b0f5a4dc9bda
Tiago Varela <vogait@gmail.com>
2014-12-15 20:02:38 +0000

    Add image_url to Recipe

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20141215181507) do
     t.string   "url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_url"
   end
 
   create_table "votes", force: true do |t|
